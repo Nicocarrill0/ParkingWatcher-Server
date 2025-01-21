be95f7627bdf92558c14744822f0dc4f5e16d448
Francis Li <mail@francisli.com>
2025-01-21 12:27:56 -0800

    Add openapi doc gen with fastify/swagger, scalar, and zod. Also add neostandard linter.

--- routes/example/index.js
+++ routes/example/index.js
@@ -1,5 +1,17 @@
+import { StatusCodes } from 'http-status-codes';
+import { z } from 'zod';
+
 export default async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return 'this is an example'
-  })
+  fastify.get('/',
+    {
+      schema: {
+        description: 'This is example API endpoint',
+        response: {
+          [StatusCodes.OK]: z.string(),
+        },
+      },
+    },
+    async function (request, reply) {
+      return 'this is an example';
+    });
 }
